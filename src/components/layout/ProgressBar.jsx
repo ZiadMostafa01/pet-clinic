1da394f0f4cd55c199cddd8f7d6236463af6687b
ZiadMostafa01 <ziadmostafa9866@gmail.com>
2026-02-15 01:09:20 +0200

    Initial commit for new repo

---
+++ src/components/layout/ProgressBar.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useRef } from "react";
+
+function ProgressBar() {
+  const containerRef = useRef(null);
+  const progressRef = useRef(null);
+
+  useEffect(() => {
+    let animationFrame;
+
+    const updateProgress = () => {
+      const scrollTop = window.scrollY;
+      const scrollHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+
+      const scrolledPercent = Math.min(
+        Math.max((scrollTop / scrollHeight) * 100, 0),
+        100,
+      );
+
+      if (progressRef.current) {
+        progressRef.current.style.width = `${scrolledPercent}%`;
+      }
+
+      if (containerRef.current) {
+        containerRef.current.style.height = scrollTop > 0 ? "4px" : "0px";
+        containerRef.current.style.opacity = scrollTop > 0 ? "1" : "0";
+      }
+
+      animationFrame = requestAnimationFrame(updateProgress);
+    };
+
+    animationFrame = requestAnimationFrame(updateProgress);
+    return () => cancelAnimationFrame(animationFrame);
+  }, []);
+
+  return (
+    <div
+      ref={containerRef}
+      className="w-full bg-blue-100 overflow-hidden transition-all duration-400 ease-out h-0 opacity-0"
+    >
+      <div
+        ref={progressRef}
+        className="h-1 bg-[var(--primary)] transition-all duration-75 ease-out"
+        style={{ width: "0%" }}
+      />
+    </div>
+  );
+}
+
+export default ProgressBar;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+import { Outlet } from "react-router-dom";
+
+function MainLayout() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default MainLayout;
